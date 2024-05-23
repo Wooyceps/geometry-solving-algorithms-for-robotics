4f826e8a49c7769c3bc919046afabdf960b2a2ba
Michał Wójcik <mwooycik@gmail.com>
2024-05-23 22:49:01 +0200

    working on remapping alorithm

---
+++ remapping.py
@@ -0,0 +1,15 @@
+WIDTH = 100
+HEIGHT = 50
+
+TARGET_WIDTH = 10
+TARGET_HEIGHT = 5
+
+w_ratio = WIDTH // TARGET_WIDTH # 10
+h_ratio = HEIGHT // TARGET_HEIGHT # 10
+
+def populate(i,j):
+    if
+
+
+
+mapped_grid = [[populate(i,j) for j in range(TARGET_WIDTH)] for i in range(TARGET_HEIGHT)]
